e38bad67d9a5de7bac62741aa8456892377572ac
Daniel Cordero <dn.lambb@gmail.com>
2023-02-06 07:47:14 -0300

    chore: config tailwind colors and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			heading: ["Raleway", "system-ui"],
+			body: ["Roboto Mono", "system-ui"]
+		},
+		colors: {
+			"color-1": "#ffffff",
+			"color-2": "#1a1c20",
+			"color-3": "#757575",
+			"color-4": "#f0a500",
+		},
 		extend: {},
 	},
 	plugins: [],
