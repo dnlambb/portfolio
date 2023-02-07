40c0a70f047ea24dd1381aa4fc81cf498dcb1158
Daniel Cordero <dn.lambb@gmail.com>
2023-02-07 20:04:43 -0300

    refactor(Link): add target blank

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -5,7 +5,7 @@ const Link = ({ href, children }) => {
 		<a
 			href={href}
 			className="w-fit flex gap-3 items-center hover:underline underline-offset-2 decoration-color-4 transition-all duration-300"
-			target={"_blank"}
+			target="_blank"
 		>
 			{children}
 		</a>
@@ -14,7 +14,7 @@ const Link = ({ href, children }) => {
 
 const LinkSocial = ({ href, children }) => {
 	return (
-		<a href={href} className="w-fit hover:scale-110 transition-all cursor-pointer">
+		<a href={href} className="w-fit hover:scale-110 transition-all cursor-pointer" target="_blank">
 			{children}
 		</a>
 	);
@@ -23,8 +23,9 @@ const LinkSocial = ({ href, children }) => {
 const LinkArrow = ({ href, content }) => {
 	return (
 		<a
-			class="w-fit m-auto text-2xl text-center hover:underline underline-offset-4 decoration-color-4 flex flex-row-reverse items-center gap-3 group"
 			href={href}
+			class="w-fit m-auto text-2xl text-center hover:underline underline-offset-4 decoration-color-4 flex flex-row-reverse items-center gap-3 group"
+			target="_blank"
 		>
 			<IconArrow fill="#fff" size={30} classname="group-hover:translate-x-3 transition-all" />
 			{content}
