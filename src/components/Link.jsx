371b794fe93d992663064a71f9d90dc692a2e887
Daniel Cordero <dn.lambb@gmail.com>
2023-02-07 20:03:25 -0300

    fix: typo in href prop of Link Social

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -14,7 +14,7 @@ const Link = ({ href, children }) => {
 
 const LinkSocial = ({ href, children }) => {
 	return (
-		<a hrefLang={href} className="w-fit hover:scale-110 transition-all cursor-pointer">
+		<a href={href} className="w-fit hover:scale-110 transition-all cursor-pointer">
 			{children}
 		</a>
 	);
@@ -26,7 +26,7 @@ const LinkArrow = ({ href, content }) => {
 			class="w-fit m-auto text-2xl text-center hover:underline underline-offset-4 decoration-color-4 flex flex-row-reverse items-center gap-3 group"
 			href={href}
 		>
-			<IconArrow fill="#fff" size={30} classname="group-hover:translate-x-3 transition-all"/>
+			<IconArrow fill="#fff" size={30} classname="group-hover:translate-x-3 transition-all" />
 			{content}
 		</a>
 	);
