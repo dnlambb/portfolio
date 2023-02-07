91356bee778159c64dd86dd1b6ebc0584f2d0537
Daniel Cordero <dn.lambb@gmail.com>
2023-02-07 17:33:15 -0300

    feat: add resume component

---
+++ src/components/Resume.jsx
@@ -0,0 +1,13 @@
+import { IconResume } from "./Icons";
+
+export const Resume = () => {
+	return (
+		<a
+			href="@assets/button"
+			className="w-fit h-fit mt-6 text-xl flex items-center gap-2 transition-all rounded-lg group hover:underline decoration-color-4 underline-offset-4"
+		>
+			<IconResume fill="#fff" size={24} />
+			Resume
+		</a>
+	);
+};
