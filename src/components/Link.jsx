0abba9fc0eb33182320d64b4b2b3f321ef90b6ff
Daniel Cordero <dn.lambb@gmail.com>
2023-02-08 19:03:27 -0300

    feat(Link): add new prop for target blank

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,31 +1,35 @@
 import { IconArrow } from "./Icons";
 
-const Link = ({ href, children }) => {
+const Link = ({ href, children, newPage = false }) => {
 	return (
 		<a
 			href={href}
 			className="w-fit flex gap-3 items-center hover:underline underline-offset-2 decoration-color-4 transition-all duration-300"
-			target="_blank"
+			target={newPage === true ? "_blank" : ""}
 		>
 			{children}
 		</a>
 	);
 };
 
-const LinkSocial = ({ href, children }) => {
+const LinkSocial = ({ href, children, newPage = false }) => {
 	return (
-		<a href={href} className="w-fit hover:scale-110 transition-all cursor-pointer" target="_blank">
+		<a
+			href={href}
+			className="w-fit hover:scale-110 transition-all cursor-pointer"
+			target={newPage === true ? "_blank" : ""}
+		>
 			{children}
 		</a>
 	);
 };
 
-const LinkArrow = ({ href, content }) => {
+const LinkArrow = ({ href, content, newPage = false }) => {
 	return (
 		<a
 			href={href}
 			class="w-fit m-auto text-2xl text-center hover:underline underline-offset-4 decoration-color-4 flex flex-row-reverse items-center gap-3 group"
-			target="_blank"
+			target={newPage === true ? "_blank" : ""}
 		>
 			<IconArrow fill="#fff" classname="w-7 group-hover:translate-x-3 transition-all" />
 			{content}
