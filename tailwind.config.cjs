db5f5a421a2e5abd32f92b1a00816c200a23a21d
Daniel Cordero <dn.lambb@gmail.com>
2023-02-08 21:37:40 -0300

    feat: add transparent bg for the navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			heading: ['Raleway', 'system-ui'],
-			body: ['Roboto Mono', 'system-ui'],
+			heading: ["Raleway", "system-ui"],
+			body: ["Roboto Mono", "system-ui"],
 		},
 		colors: {
-			'color-1': '#ffffff',
-			'color-2': '#1a1c20',
-			'color-3': '#292733',
-			'color-4': '#f0a500',
+			"color-1": "#ffffff",
+			"color-2": "#1a1c20",
+			"color-3": "#292733",
+			"color-4": "#f0a500",
+			"navbar": "rgba(0,0,0,0.8)",
 		},
 		extend: {},
 	},
