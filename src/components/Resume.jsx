45ebd6c6bf2ebb9f0f54e8efe1c568ea5743dfa8
Daniel Cordero <dn.lambb@gmail.com>
2023-02-08 22:28:06 -0300

    fix(Resume): change path link to the resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,7 @@ import { IconResume } from "./Icons";
 export const Resume = () => {
 	return (
 		<a
-			href="../../public/assets/Daniel Cordero CV.pdf"
+			href="/assets/Daniel%20Cordero%20CV.pdf"
 			className="w-fit h-fit mt-6 text-xl flex items-center gap-2 transition-all rounded-lg group hover:underline decoration-color-4 underline-offset-4"
 		>
 			<IconResume fill="#fff" size={24} />
