477e9f284d3f2e7bd46e462af72f790043f2463a
Daniel Cordero <dn.lambb@gmail.com>
2023-02-11 01:24:57 -0300

    refactor(footer): improve responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FooterLink } from "./Link";
 export const Footer = () => {
 	return (
 		<footer className="mt-20 px-6">
-			<ul className="max-w-fit mx-auto flex flex-wrap gap-5">
+			<ul className="max-w-fit mx-auto grid grid-cols-2 gap-5 sm:flex">
 				<FooterLink href="https://github.com/dnlambb/portfolio">Repository</FooterLink>
 				<FooterLink href="https://www.linkedin.com/in/daniel-cordero-meléndez/">
 					Linkedin
@@ -11,7 +11,7 @@ export const Footer = () => {
 				<FooterLink href="https://github.com/dnlambb">GitHub</FooterLink>
 				<FooterLink href="mailto:dnlambb@writeme.com?Subject=Hello">Email</FooterLink>
 			</ul>
-			<p className="my-6 text-center">Built by Daniel Cordero</p>
+			<p className="my-6 text-center pt-6">Built by Daniel Cordero</p>
 		</footer>
 	);
 };
